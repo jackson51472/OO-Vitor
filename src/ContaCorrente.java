38a198dfc377334d04535ff86066cc16205c2413
jackson51472 <53409735+jackson51472@users.noreply.github.com>
2022-11-29 15:29:14 -0300

    Update ContaCorrente.java

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,7 +1,31 @@
-public class ContaCorrente extends Conta {
-    private Double limite;
+public class ContaCorrente {
 
-    public ContaCorrente(String numero, Double valorCaixa) {
-        super(numero, valorCaixa);
+    private Double limite;
+    private Double saldo;
+
+    public void calcularSaldo(){
+
+        System.out.println("Valor em caixa;" + getSaldo());
+
+    }
+
+    public void depositar(String numeroConta, Double valor){
+        
+    }
+
+    public Double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(Double limite) {
+        this.limite = limite;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
     }
 }
